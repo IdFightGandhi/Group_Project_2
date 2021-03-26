1d8ae5840aabe29996c590c220d1ff0ab8b2fa13
Steven Moseni <smoseni@hotmail.com>
2021-03-26 11:22:38 -0700

    added views folder / handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -38,13 +38,13 @@ router.get('/userProfile', async (req, res) => {
 
 
 //  // Serialize user data so templates can read it
-//  const users = userData.map((project) => project.get({ plain: true }));
+const users = userData.map((project) => project.get({ plain: true }));
 
- // Pass serialized data into Handlebars.js template
-//  res.render('homepage', { users });
-// } catch (err) {
-//  res.status(500).json(err);
-// }
-// });
+// Pass serialized data into Handlebars.js template
+res.render('homepage', { users });
+} catch (err) {
+res.status(500).json(err);
+}
+});
 
 module.exports = router;
