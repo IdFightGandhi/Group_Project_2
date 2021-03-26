b2d5897cdb2108e1e27a314fdd4c1ec1137cda3d
Steven Moseni <smoseni@hotmail.com>
2021-03-26 10:30:08 -0700

    created routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,5 +8,43 @@ router.get('/', async (req, res) => {
 
 });
 
+router.get('/signup', async (req, res) => {
+
+    res.json({message: 'you are in signup'});
+
+});
+
+router.get('/profile', async (req, res) => {
+
+    res.json({message: 'you are in profile'});
+
+});
+
+router.get('/search', async (req, res) => {
+
+    res.json({message: 'you are in search'});
+
+});
+
+router.get('/userProfile', async (req, res) => {
+
+    res.json({message: 'you are in user profile'});
+
+});
+
+
+
+//TODO for each r
+
+
+//  // Serialize user data so templates can read it
+//  const users = userData.map((project) => project.get({ plain: true }));
+
+ // Pass serialized data into Handlebars.js template
+//  res.render('homepage', { users });
+// } catch (err) {
+//  res.status(500).json(err);
+// }
+// });
 
 module.exports = router;
