f3c0c199ed7c39e409f824657725ea3999378f82
Steven Moseni <smoseni@hotmail.com>
2021-03-26 11:40:59 -0700

    updated homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,48 +3,57 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
+     // res.json({message: 'you are in home routes'});
 
-    res.json({message: 'you are in home routes'});
+    try {
+        res.render('homepage');
+      } catch (err) {
+        res.status(500).json(err);
+      }
 
 });
 
 router.get('/signup', async (req, res) => {
+    // res.json({message: 'you are in signup'});
 
-    res.json({message: 'you are in signup'});
+    try {
+        res.render('/signup');
+      } catch (err) {
+        res.status(500).json(err);
+      }
 
 });
 
 router.get('/profile', async (req, res) => {
-
-    res.json({message: 'you are in profile'});
+    // res.json({message: 'you are in profile'});
+    try {
+        res.render('/profile');
+      } catch (err) {
+        res.status(500).json(err);
+      }
 
 });
 
 router.get('/search', async (req, res) => {
-
-    res.json({message: 'you are in search'});
+    // res.json({message: 'you are in search'});
+    try {
+        res.render('/search');
+      } catch (err) {
+        res.status(500).json(err);
+      }
 
 });
 
 router.get('/userProfile', async (req, res) => {
-
-    res.json({message: 'you are in user profile'});
+     // res.json({message: 'you are in user profile'});
+     try {
+        res.render('/userProfile');
+      } catch (err) {
+        res.status(500).json(err);
+      }
 
 });
 
 
-
-//TODO for each r
-
-
-//  // Serialize user data so templates can read it
-const users = userData.map((project) => project.get({ plain: true }));
-
-// Pass serialized data into Handlebars.js template
-res.render('homepage', { users });
-} catch (err) {
-res.status(500).json(err);
-}
-});
 
 module.exports = router;
